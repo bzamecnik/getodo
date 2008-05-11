cfadfe9b17e22394e0f12b898f02a906ae499633
bohumir.zamecnik <bohumir.zamecnik@a6c46999-b849-0410-ad42-e14e6507255a>
2008-05-11 20:56:51 +0000

    Added declarations of some Task and TaskManager member functions and moved to include/getodo.h.

--- playground/getodo-sqlite.sql
+++ playground/getodo-sqlite.sql
@@ -11,7 +11,7 @@
 -- Vytvoreni tabulek --
 
 CREATE TABLE Task (
-taskID      INTEGER      NOT NULL,
+taskId      INTEGER      NOT NULL,
 description      STRING      NOT NULL,
 longDescription      STRING      NOT NULL,
 dateCreated      STRING      NOT NULL,
@@ -22,24 +22,24 @@ estDuration      STRING,
 recurrence      STRING,
 priority      STRING      NOT NULL,
 completedPercentage      INTEGER      DEFAULT '0'  NOT NULL,
-CONSTRAINT pk_Task PRIMARY KEY (taskID));
+CONSTRAINT pk_Task PRIMARY KEY (taskId));
 
 CREATE TABLE Tag (
-tagID      INTEGER      NOT NULL,
+tagId      INTEGER      NOT NULL,
 tagName      STRING      NOT NULL  UNIQUE,
-CONSTRAINT pk_Tag PRIMARY KEY (tagID));
+CONSTRAINT pk_Tag PRIMARY KEY (tagId));
 
 CREATE TABLE Subtask (
-sub_taskID      INTEGER      NOT NULL,
-super_taskID      INTEGER      NOT NULL,
-CONSTRAINT pk_Subtask PRIMARY KEY (sub_taskID, super_taskID),
-CONSTRAINT fk_Subtask_Task FOREIGN KEY (sub_taskID, super_taskID) REFERENCES Task(taskID,taskID));
+sub_taskId      INTEGER      NOT NULL,
+super_taskId      INTEGER      NOT NULL,
+CONSTRAINT pk_Subtask PRIMARY KEY (sub_taskId, super_taskId),
+CONSTRAINT fk_Subtask_Task FOREIGN KEY (sub_taskId, super_taskId) REFERENCES Task(taskId,taskId));
 
 CREATE TABLE Tagged (
-taskID      INTEGER      NOT NULL,
-tagID      INTEGER      NOT NULL,
-CONSTRAINT pk_Tagged PRIMARY KEY (taskID, tagID),
-CONSTRAINT fk_Tagged_Task FOREIGN KEY (taskID) REFERENCES Task(taskID),
-CONSTRAINT fk_Tagged_Tag FOREIGN KEY (tagID) REFERENCES Tag(tagID));
+taskId      INTEGER      NOT NULL,
+tagId      INTEGER      NOT NULL,
+CONSTRAINT pk_Tagged PRIMARY KEY (taskId, tagId),
+CONSTRAINT fk_Tagged_Task FOREIGN KEY (taskId) REFERENCES Task(taskId),
+CONSTRAINT fk_Tagged_Tag FOREIGN KEY (tagId) REFERENCES Tag(tagId));
 
 -- Konec skriptu --
