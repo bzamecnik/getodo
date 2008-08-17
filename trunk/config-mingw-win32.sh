70b615fb59f7befbc7d90433a15b77b3c1fc8ee1
bohumir.zamecnik <bohumir.zamecnik@a6c46999-b849-0410-ad42-e14e6507255a>
2008-08-17 15:40:02 +0000

    Moving from Autotools to Visual Studio. Autotools version is saved as a branch for future use.

--- trunk/config-mingw-win32.sh
+++
@@ -1,4 +0,0 @@
-# ./configure invokation with Boost and SQLite in non-standard locations
-CPPFLAGS=-I/usr/local/include ./configure \
-  --with-boost-libdir=/usr/local/lib \
-  --with-sqlite3=/usr/local
